00bfbe500f216d5de0c1f23c6d384ccc9d2df316
Dilan Fernando <dilan.fd@gmail.com>
2017-12-02 14:57:53 -0600

    solution to problem 1

---
+++ mult_3_or_5_to_N.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def mult_of_3_or_5_upto_N(N):
+    """
+    Finds the sum of the multiples of 3 or 5 upto N
+    Excluding N.
+
+    Procedure:
+    ---------
+    Examples. The multiples of 3 or 5 upto 10 excluding
+    10 are 3, 5, 6, 9. The sum of these values is 23.
+
+    Input:
+    -----
+    N: Natural number (excluding zero)
+
+    Output:
+    -----
+    The sum of the multiples of 3 or 5 upto but excluding
+    N
+    """
+    list_of_numbers_to_N = np.arange(1, N, 1)
+    mult_of_3_or_5 = [n for n in list_of_numbers_to_N if
+                      (n % 3 == 0 or n % 5 == 0)]
+    return sum(mult_of_3_or_5)
+
+
+def main():
+    return print(mult_of_3_or_5_upto_N(1000))
+
+
+if __name__ == '__main__':
+    main()
